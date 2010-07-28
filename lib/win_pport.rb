08f4702ed31bb0c79bed474636d4334380a3586f
scottaj <aljscott1990@gmail.com>
2010-07-28 15:05:29 -0600

    Changed to MIT style license

--- lib/win_pport.rb
+++ lib/win_pport.rb
@@ -1,18 +1,6 @@
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# Copyright(c) 2010 Al Scott
+# License details can be found in the LICENSE file.
 
-# Class for interacting with a parallel port in microsoft windows using the
-# inpout32 DLL.
 begin
     require 'Win32API'
 rescue LoadError
